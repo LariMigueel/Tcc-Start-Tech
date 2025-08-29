1c5c5ef8f869c891b77991a2a147d5993c3842f8
HigorFequer <higorfequer@gmail.com>
2025-08-29 19:40:35 -0300

    feat: update app title and add new components
    - Changed app title from 'tcc' to 'teste-tcc' in app.component.ts and updated related tests.
    - Added routing for HomeComponent in app.routes.ts.
    - Created FuncoesComponent with basic template and styles.
    - Created LoginComponent with form for user authentication.
    - Created SobreComponent with information about the application.
    - Updated index.html to use app-login and router outlet.
    - Added global styles in styles.css.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tcc' title`, () => {
+  it(`should have the 'teste-tcc' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tcc');
+    expect(app.title).toEqual('teste-tcc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tcc');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, teste-tcc');
   });
 });
