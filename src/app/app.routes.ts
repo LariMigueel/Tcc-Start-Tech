ba8bb8a1b464e6960a900dd37db540007b05f3dd
HigorFequer <higorfequer@gmail.com>
2025-08-29 19:46:34 -0300

    feat: update routing to use LoginComponent instead of HomeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 export const routes: Routes = [
     {
 
         path:"",
-        component:HomeComponent
+        component:LoginComponent
     }
 ];
